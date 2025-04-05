01a50427bc602e9d487a080a73d85b5724a14e25
anonymousProgrammer53 <aprogrammer53@gmail.com>
2025-04-05 23:09:20 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let getIconMenu = document.querySelector('img[alt="icon-menu"]');
+getIconMenu.addEventListener('click', () => {
+    let getNavList = document.querySelector('header')
+        .querySelector('div').querySelector('nav').querySelector('ul');
+    if (getNavList.classList.contains('d-none-768')) {
+        getNavList.classList.replace('d-none-768', 'd-flex-768');
+        console.log(getIconMenu.src = './public/assets/images/icon-menu-close.svg');
+    } else if (getNavList.classList.contains('d-flex-768')) {
+        getNavList.classList.replace('d-flex-768', 'd-none-768');
+        console.log(getIconMenu.src = './public/assets/images/icon-menu.svg');
+    }
+});
